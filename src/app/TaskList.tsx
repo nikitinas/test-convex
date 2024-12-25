d34a2b9aa5e671bac5272879607c5090f537fecb
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2024-12-25 14:04:11 +0300

    Add styles for delete icon

--- src/app/TaskList.tsx
+++ src/app/TaskList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useQuery, Authenticated, useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { Id } from '../../convex/_generated/dataModel';
+import { DeleteIcon } from '../resources/icons';
 
 const TaskList = () => {
     const tasks = useQuery(api.tasks.get);
@@ -20,22 +21,25 @@ const TaskList = () => {
         <Authenticated>
             <div className="task-list">
                 {tasks?.map(({ _id, text, isCompleted }) => (
-                    <div key={_id} className="task-item flex items-center p-2 mb-2 border-b border-gray-300">
-                        <input
-                            type="checkbox"
-                            id={_id}
-                            className="mr-2"
-                            checked={isCompleted}
-                            onChange={() => handleCheckboxChange(_id, isCompleted)}
-                        />
-                        <label htmlFor={_id} className={`task-text ${isCompleted ? 'line-through text-gray-500' : ''}`}>
-                            {text}
-                        </label>
+                    <div key={_id} className="task-item flex items-center justify-between p-2 mb-2 border-b border-gray-300">
+                        <div className="flex items-center">
+                            <input
+                                type="checkbox"
+                                id={_id}
+                                className="mr-2"
+                                checked={isCompleted}
+                                onChange={() => handleCheckboxChange(_id, isCompleted)}
+                            />
+                            <label htmlFor={_id} className={`task-text ${isCompleted ? 'line-through text-gray-500' : ''}`}>
+                                {text}
+                            </label>
+                        </div>
                         <button
                             onClick={() => handleDeleteTask(_id)}
                             className="ml-2 text-red-500 hover:text-red-700"
+                            aria-label="Delete task"
                         >
-                            Delete
+                            <DeleteIcon />
                         </button>
                     </div>
                 ))}

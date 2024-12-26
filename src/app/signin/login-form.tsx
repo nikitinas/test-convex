9f738276e25caa4d86cb00d7fda25acb14cda0c5
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2024-12-26 15:27:21 +0300

    cleanup

--- src/app/signin/login-form.tsx
+++ src/app/signin/login-form.tsx
@@ -101,7 +101,7 @@ export function LoginForm({
                 </>
             ) : (
                 <div>
-                    <p>Проверьте почту <strong>{email}</strong> и нажмите в письме на кнопку для подтверждения входа.</p>
+                    <p>Проверьте почту <strong>{email}</strong> и нажмите в письме на кнопку &quot;Войти&quot;.</p>
                     <a href="#" onClick={() => setStep("signIn")} className="text-blue-500 hover:underline flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 mr-1">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />

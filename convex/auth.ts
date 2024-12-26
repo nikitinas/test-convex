dda5fafb360c62017a03e9603423057c231ca0d2
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2024-12-26 11:56:25 +0300

    Fallback to recommended path of vk provider

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,11 +1,10 @@
-import { ProfileCallback } from "@auth/core/providers";
 import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
-import VK, { VkProfile } from "@auth/core/providers/vk";
+import VK from "@auth/core/providers/vk";
 import Yandex from "@auth/core/providers/yandex";
 import { convexAuth } from "@convex-dev/auth/server";
 
-const apiVersion = "5.199"
+const apiVersion = "5.131"
 
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
@@ -15,29 +14,54 @@ export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
     Google,
     {
       ...VK({ checks: [] }), // Fix: PKCE is unsupported for server-side authorization
-      token: {
-        url: `https://oauth.vk.com/access_token?v=${apiVersion}`,
-        conform: async (response: Response) => {
-          const data = await response.json()
-          return new Response(
-            // Fix: OperationProcessingError: "response" body "token_type" property must be a string
-            JSON.stringify({
-              token_type: 'dpop',
-              ...data,
-            }),
-            // Fix: OperationProcessingError: "response" content-type must be application/json
-            { headers: { "content-type": "application/json" }, status: response.status }
-          )
+      ...{
+        userinfo: {
+          //Problem is that VK provider works incorrectly
+          url: `https://api.vk.com/method/users.get?fields=photo_100&v=${apiVersion}`,
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          async request(context: any) {
+            /*
+              Here we store email of user if it presented, because user.get don't return it.
+           */
+            const email = context.tokens.email || undefined;
+            const access_token = context.tokens.access_token;
+            const f = await fetch(
+              `https://api.vk.com/method/users.get?fields=photo_100&v=${apiVersion}&access_token=${access_token}`
+            );
+            return { ...(await f.json()), email: email };
+          },
+        },
+        profile(result) {
+          /*
+           * Again, default VK provider just works not as expected, it is why we need to save user data, and also
+           * adds email to user.
+           *
+           * */
+          const profile = result.response?.[0] ?? (result.id ? result : {});
+          return {
+            id: profile.id.toString(),
+            name: [profile.first_name, profile.last_name].filter((v) => !!v).join(' '),
+            email: result.email || null,
+            image: profile.photo_100,
+          };
+        },
+        token: {
+          url: `https://oauth.vk.com/access_token?v=${apiVersion}`,
+          conform: async (r: Response) => {
+            /*
+             * And yet again base VK provider just don't working.
+             * */
+            const resp = await r.json();
+            return new Response(
+              JSON.stringify({
+                token_type: 'dpop',
+                ...resp,
+              }),
+              { headers: { "Content-Type": "application/json" }, status: r.status },
+            );
+          },
         }
-      },
-      profile(profile: VkProfile) {
-        return {
-          id: profile.id.toString(),
-          name: [profile.first_name, profile.last_name].filter(Boolean).join(" "),
-          email: profile.email ?? null,
-          image: profile.photo_100,
-        }
-      },
+      }
     }
   ],
 });

4e1c37153480f7671626fc2695e8507e3e41bbc2
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2024-12-26 11:01:14 +0300

    Add yandex auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,41 @@
+import { ProfileCallback } from "@auth/core/providers";
 import GitHub from "@auth/core/providers/github";
+import VK, { VkProfile } from "@auth/core/providers/vk";
+import Yandex from "@auth/core/providers/yandex";
 import { convexAuth } from "@convex-dev/auth/server";
 
+const apiVersion = "5.199"
+
+
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [GitHub],
+  providers: [
+    GitHub,
+    Yandex,
+    {
+      ...VK({ checks: [] }), // Fix: PKCE is unsupported for server-side authorization
+      token: {
+        url: `https://oauth.vk.com/access_token?v=${apiVersion}`,
+        conform: async (response: Response) => {
+          const data = await response.json()
+          return new Response(
+            // Fix: OperationProcessingError: "response" body "token_type" property must be a string
+            JSON.stringify({
+              token_type: 'dpop',
+              ...data,
+            }),
+            // Fix: OperationProcessingError: "response" content-type must be application/json
+            { headers: { "content-type": "application/json" }, status: response.status }
+          )
+        }
+      },
+      profile(profile: VkProfile) {
+        return {
+          id: profile.id.toString(),
+          name: [profile.first_name, profile.last_name].filter(Boolean).join(" "),
+          email: profile.email ?? null,
+          image: profile.photo_100,
+        }
+      },
+    }
+  ],
 });
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -6,19 +6,31 @@ export default function SignIn() {
     const { signIn } = useAuthActions();
 
     // Extracted click handler function
-    const handleSignIn = () => {
-        console.log("Signing in with GitHub");
-        void signIn("github", { redirectTo: "/" });
+    const handleSignIn = (provider: string) => {
+        console.log(`Signing in with ${provider}`);
+        void signIn(provider, { redirectTo: "/" });
     };
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <button
-                onClick={handleSignIn} // Updated to use the new handler
+                onClick={() => handleSignIn("github")} // Updated to use the new handler
                 className="sign-in-button"
             >
                 Sign in with GitHub
             </button>
-        </main>
+            <button
+                onClick={() => handleSignIn("yandex")} // Updated to use the new handler
+                className="sign-in-button"
+            >
+                Sign in with Yandex
+            </button>
+            <button
+                onClick={() => handleSignIn("vk")} // Updated to use the new handler
+                className="sign-in-button"
+            >
+                Sign in with VK
+            </button>
+        </main >
     );
 }
